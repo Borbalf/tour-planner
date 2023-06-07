5e4e99e3a84874857ac30fdf9b2842f769a2bc1a
Matteo Bobbio <matteo@eutopiagreen.com>
2023-06-07 19:35:03 +0200

    adding data to skyCondition and User

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,10 @@ INSERT INTO City (name, latitude, longitude, altitude) VALUES ('Genova', 44.5, 8
 INSERT INTO City (name, latitude, longitude, altitude) VALUES ('Savona', 44.5, 8.98, 0);
 INSERT INTO City (name, latitude, longitude, altitude) VALUES ('Imperia', 44.5, 8.98, 0);
 INSERT INTO City (name, latitude, longitude, altitude) VALUES ('La Spezia', 44.5, 8.98, 0);
+
+INSERT INTO SkyCondition (condition) VALUES ('CLEAR_SKY');
+INSERT INTO SkyCondition (condition) VALUES ('CLOUDY');
+INSERT INTO SkyCondition (condition) VALUES ('RAIN_SHOWERS');
+INSERT INTO SkyCondition (condition) VALUES ('SNOW_SHOWERS');
+
+INSERT INTO `User` (name, surname, email) VALUES ('Mario', 'Rossi', 'mario@rossi.com');
